7337ab60c3f835286ebb0c9577828ab7a0a2f86e
Schubert <rfswdp@gmail.com>
2022-08-11 20:05:39 -0300

    updated dev secret

--- arcdjango/settings.py
+++ arcdjango/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gk$y21akn8=bvijoqk&1a%wn=yb)@qo40bm1c!*a+$4%56w%jl'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', True))
