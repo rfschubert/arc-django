93133cb9023f411cd20ffd2098153c2291319ae3
Schubert <rfswdp@gmail.com>
2022-08-11 17:26:09 -0300

    configured settings.py

--- arcdjango/settings.py
+++ arcdjango/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gk$y21akn8=bvijoqk&1a%wn=yb)@qo40bm1c!*a+$4%56w%jl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', True))
 
 ALLOWED_HOSTS = []
 
@@ -74,9 +75,13 @@ WSGI_APPLICATION = 'arcdjango.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default"  : {
+        "ENGINE"  : os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME"    : os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER"    : os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST"    : os.environ.get("SQL_HOST", "localhost"),
+        "PORT"    : os.environ.get("SQL_PORT", "5432"),
     }
 }
 
